4472f53580a841c5943e29d8a1324a102c92b5e8
张晓甜 <506834853@qq.com>
2017-03-29 10:53:34 +0800

    s

--- master-out-back-app.js
+++ master-out-back-app.js
@@ -1,11 +1,13 @@
 //本插件需要 outbackapp.vue 组件
 //为方便修改 组件文件 还是自己重新制作为好 其实只是为了 全局使用 backAppFn
+import OutBackAppComment from 'outbackapp';
 
 const MasterOutBackApp = {}
 
 MasterOutBackApp.install = function (Vue, options) {
+  let comment=options.comment||OutBackAppComment
   let $vm;
-  const BackApp = Vue.extend(options.comment)
+  const BackApp = Vue.extend(comment)
   $vm = new BackApp({
     el: document.createElement('div')
   })
